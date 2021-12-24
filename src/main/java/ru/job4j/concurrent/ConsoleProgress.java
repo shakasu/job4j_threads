13a1208d381b67f9efda3b6d88597850a7c10ead
karen.shakhnazaryan <olga.shultseva@x5.ru>
2021-12-24 19:23:15 +0300

    3.1. Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -21,7 +21,7 @@ public class ConsoleProgress implements Runnable {
                 System.out.print("Loading ... \\\r");
                 Thread.sleep(500);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
         }
     }
