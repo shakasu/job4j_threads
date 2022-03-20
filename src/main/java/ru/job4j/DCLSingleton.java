56a9b44785bfe7581def31660a597493d2414a7c
Karen Shakhnazaryan <kshakhnazaryan@innodata.ru>
2022-03-20 15:04:15 +0300

    Модель памяти Java [#267917]

--- src/main/java/ru/job4j/DCLSingleton.java
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -1,16 +1,20 @@
 package ru.job4j;
 
 public final class DCLSingleton {
-    
+
     private static volatile DCLSingleton inst;
-    
+
     private DCLSingleton() {
-    
+
     }
-    
+
     public static DCLSingleton instOf() {
         if (inst == null) {
-            inst = new DCLSingleton();
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
         }
         return inst;
     }
